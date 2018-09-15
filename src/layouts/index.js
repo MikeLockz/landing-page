379fe399c04eebfa434ae92fc1cbd1fe4977cf58
Michael Lockwitz <michaellockwitz@MBP-2018.local>
2018-09-15 08:40:27 -0600

    Adding body font.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import '../css/inter-ui.css'
 import '../css/PxGroteskRegular-Regular.css'
 import './all.sass'
 import config from '../meta/config'
