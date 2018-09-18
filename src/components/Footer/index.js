c6bfb9004904e25a408fbead45fa362c14f50480
Michael Lockwitz <michaellockwitz@MBP-2018.local>
2018-09-17 20:36:48 -0600

    Updating footer for mobile.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,37 +1,37 @@
-  import React from 'react'
+import React from 'react'
 import Link from 'gatsby-link'
 import {
-  Footer,
-  Container,
-  Columns,
-  Column,
-  Content,
-  Icon,
+Footer,
+Container,
+Columns,
+Column,
+Content,
+Icon,
 } from 'bloomer'
 import '../../css/font-awesome.min.css'
 
 const AppFooter = () => (
-  <Footer id='footer' style={{ backgroundColor: 'inherit' }}>
-    <Container>
-      <div id='footer-hr' style={{ borderTop: 'solid 1px #33383E' }}></div>
-    </Container>
+<Footer id='footer' style={{ backgroundColor: 'inherit' }}>
+  <Container>
+    <div id='footer-hr' style={{ borderTop: 'solid 1px #33383E' }}></div>
+  </Container>
 
-    <Container>
-      <Columns className='is-mobile'  style={{ color: '#BBBEC2', paddingTop: '21px', paddingBottom: '14px' }}>
-        <Column id="copyright" isSize={{ mobile: 8, tablet: 8, desktop: 8 }}>
-          © 2018 Consensys, Inc. <Link to="privacy-policy" title="ConsenSys Privacy Policy">Privacy Policy</Link>
-        </Column>
-        <Column id="social-icons" style={{ textAlign: 'right' }} isSize={{ mobile: 4, tablet: 4, desktop: 4 }}>
-          <a href="https://twitter.com/ConsensysDesign" title='ConsenSys Product Design Circle on Twitter'>
-              <Icon className='fa fa-twitter' />
-          </a>
-          <a href="https://www.instagram.com/consensysdesign/" title='ConsenSys Product Design Circle on Instagram'>
-              <Icon className='fa fa-instagram' />
-          </a>
-        </Column>
-      </Columns>
-    </Container>
-  </Footer>
+  <Container>
+    <Columns className='is-mobile'  style={{ color: '#BBBEC2', paddingTop: '21px', paddingBottom: '14px' }}>
+      <Column id="copyright" style={{ flexGrow: '2' }}>
+        © 2018 Consensys, Inc. <Link to="privacy-policy" title="ConsenSys Privacy Policy">Privacy Policy</Link>
+      </Column>
+      <Column id="social-icons" style={{ textAlign: 'right' }} >
+        <a href="https://twitter.com/ConsensysDesign" title='ConsenSys Product Design Circle on Twitter'>
+            <Icon className='fa fa-twitter' />
+        </a>
+        <a href="https://www.instagram.com/consensysdesign/" title='ConsenSys Product Design Circle on Instagram'>
+            <Icon className='fa fa-instagram' />
+        </a>
+      </Column>
+    </Columns>
+  </Container>
+</Footer>
 )
 
 export default AppFooter
